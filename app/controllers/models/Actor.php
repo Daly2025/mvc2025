287b7184ffd636f2fe8173b809b96ce51e6dcb6c
Daly2025 <lydiafdez84@gmail.com>
2025-02-18 16:28:25 +0100

    empezamos

---
+++ app/controllers/models/Actor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Formacom\models;
+use vendor\Illuminate\Database\Eloquent\Model;
+class Actor extends Model{
+    protected $table = 'actor';
+    protected $primaryKey = 'actor_id';
+
+    
+}
+
+
+
+
+
+
+
+
+?>
